71b3b12823480dc9e1b8bab230c5be5cad3285fb
Skronak <narvak.skronak@gmail.com>
2024-05-01 22:40:32 +0200

    init

--- dist/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getOrderFromDatabase = void 0;
-function getOrderFromDatabase() {
-    return "order";
-}
-exports.getOrderFromDatabase = getOrderFromDatabase;
-//# sourceMappingURL=database.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,26 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as express from "express";
+import {addEntry, getAllEntries, updateEntry, deleteEntry} from "./database";
+import cors from "cors";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const app = express();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const port = 3000;
+const allowedOrigins = ["http://localhost:3000"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(cors(options));
+app.use(express.json());
+
+app.post("/there", (req, res) => res.status(200).send("Hey there!"));
+app.post("/entries", addEntry);
+app.get("/entries", getAllEntries);
+app.patch("/entries/:entryId", updateEntry);
+app.delete("/entries/:entryId", deleteEntry);
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+exports.app = functions.https.onRequest(app);
